8a6113555070fd52242a7d52e00badd68255da21
Bruno Herfst <hello@brunoherfst.com>
2018-07-24 17:39:09 +1000

    Update test.js

--- boilerplate/module/___moduleName___/test/test.js
+++ boilerplate/module/___moduleName___/test/test.js
@@ -1,5 +1,6 @@
 var argv = require('minimist')(process.argv.slice(2)); // Remove nodePath, scriptpath
 var tapes = require("tap-es"), targets = argv._, d = require('path').resolve(__dirname);
+tapes.reportDuration(false);
 
 tapes.add('___moduleName___', d+'/test.jsx', targets, true);
 tapes.run(d+'/results.md');
